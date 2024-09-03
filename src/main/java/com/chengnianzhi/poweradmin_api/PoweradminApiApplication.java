849a5a0b35bb81dbfe775e19f79cfa34a4eb19aa
DoChEnGzZ <dochengzz@hotmail.com>
2024-09-03 14:20:02 +0800

    init

---
+++ src/main/java/com/chengnianzhi/poweradmin_api/PoweradminApiApplication.java
@@ -0,0 +1,13 @@
+package com.chengnianzhi.poweradmin_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoweradminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoweradminApiApplication.class, args);
+	}
+
+}
